c0285ba70ba2bc316782fcb5cd9f3069361d7235
ARINZE JOACHIM OJILI <47617723+yhuakim@users.noreply.github.com>
2023-07-25 22:15:57 +0100

    initial commit

---
+++ pages/blog/[slug].jsx
@@ -0,0 +1,119 @@
+import { groq } from "next-sanity";
+import { client } from "../../lib/sanityClient";
+import { useRouter } from "next/router";
+import { urlFor } from "@/lib/sanityImageUrl";
+import { PortableText } from "@portabletext/react";
+import Image from "next/image";
+import Link from "next/link";
+
+export default function ReadPost({ data }) {
+    const router = useRouter();
+
+    if (router.isFallback) {
+        return <div>Loading...</div>;
+    }
+    const { post } = data;
+    const PortableTextComponent = {
+        types: {
+            image: ({ value }) => (
+                <Image
+                    src={urlFor(value).url()}
+                    alt="Image"
+                    className="rounded-lg"
+                    width={800}
+                    height={800}
+                />
+            ),
+        },
+    };
+
+    return (
+        <div className="h-auto xl:divide-y xl:divide-gray-200 xl:dark:divide-gray-700 dark:bg-gray-800 p-3">
+            <header className="pt-6 xl:pb-6">
+                <div className="space-y-1 text-center">
+                    <div className="space-y-10">
+                        <div>
+                            {/* <p className="text-base font-medium leading-6 text-teal-500">
+                                {new Date(post._createdAt).toISOString().split("T")[0]}
+                            </p> */}
+                        </div>
+                    </div>
+
+                    <div>
+                        <h1 className="text-3xl font-extrabold leading-9 tracking-tight text-gray-900 dark:text-gray-100 sm:text-4xl sm:leading-10 md:text-5xl md:leading-14">
+                            {post.title}
+                        </h1>
+                    </div>
+                </div>
+            </header>
+
+            <div className="divide-y divide-gray-200 pb-3 dark:divide-gray-700 xl:divide-y-0">
+                <div className="divide-y divide-gray-200 dark:text-gray-100 dark:divide-gray-700 xl:col-span-3 xl:row-span-2 xl:pb-0">
+                    <div className="prose max-w-none pb-8 pt-10 dark:prose-invert prose-lg">
+                        <PortableText
+                            value={post.body}
+                            components={PortableTextComponent}
+                        />
+                    </div>
+                </div>
+            </div>
+
+            <div className="ml-5 mb-4">
+                <Link href={"/blog"} className="bg-teal-500 dark:text-gray-100 p-2 rounded-md shadow-md">
+                    Go Home
+                </Link>
+            </div>
+        </div>
+    );
+}
+
+const postQuery = groq`
+  *[_type == "post" && slug.current == $slug][0] {
+    _id,
+    title,
+    body,
+    excerpt,
+    mainImage,
+    "slug": slug.current
+  }
+`;
+
+export async function getStaticPaths() {
+    const paths = await client.fetch(
+        groq`*[_type == "post" && defined(slug.current)][].slug.current`
+    );
+
+    const firstPost = paths[0];
+
+    /* 
+    console.log(paths); 
+    -----------------------------
+    [
+    'large-sites-with-odb',
+    'jamstack-for-enterprise-applications',
+    'next-on-netlify',
+    'dynamic-jamstack-sites-in-2021',
+    'modern-faster-netlify-functions',
+    'deep-dive-into-the-vue-composition-api-s-watch-method',
+    'how-to-rollback-a-deploy-in-2-seconds-on-netlify'
+  ]
+    */
+
+    return {
+        // paths: paths.map((slug) => ({ params: { slug } })),
+        paths: [{ params: { slug: firstPost } }],
+        fallback: true,
+    };
+}
+
+export async function getStaticProps({ params }) {
+    const post = await client.fetch(postQuery, {
+        slug: params.slug,
+    });
+
+    return {
+        props: {
+            data: { post },
+        },
+    };
+}
---
+++ pages/blog/index.jsx
@@ -0,0 +1,78 @@
+import Head from "next/head";
+import { groq } from "next-sanity";
+import { client } from "../../lib/sanityClient";
+import Link from "next/link";
+
+
+export default function Post({ data }) {
+    const { post } = data;
+    return (
+        <div>
+            <Head>
+                <title>Blog</title>
+                <meta name="description" content="Read our blog posts" />
+                <link rel="icon" href="/favicon.ico" />
+            </Head>
+            <section className="text-gray-600 body-font overflow-hidden">
+                <div className="container px-5 py-24 mx-auto">
+                    <div className="-my-8 divide-y-2 divide-gray-100">
+                        {post.map((blogpost) => (
+                            <div key={blogpost.slug.current}>
+                                <div className="py-8 flex flex-wrap md:flex-nowrap">
+                                    <div className="md:w-64 md:mb-0 mb-6 flex-shrink-0 flex flex-col">
+                                        <span className="font-semibold title-font text-gray-700">
+                                            {blogpost.category || "tutorial"}
+                                        </span>
+                                        <span className="mt-1 text-gray-500 text-sm">
+                                            {blogpost._createdAt}
+                                        </span>
+                                    </div>
+                                    <div className="md:flex-grow">
+                                        <h2 className="text-2xl font-medium text-gray-900 title-font mb-2">
+                                            {blogpost.title}
+                                        </h2>
+                                        <p className="leading-relaxed">{blogpost.excerpt}</p>
+                                        <Link
+                                            legacyBehavior
+                                            href={`/blog/${encodeURIComponent(
+                                                blogpost.slug.current
+                                            )}`}
+                                        >
+                                            <a className="text-green-500 inline-flex items-center mt-4">
+                                                <svg
+                                                    className="w-4 h-4 ml-2"
+                                                    viewBox="0 0 24 24"
+                                                    stroke="currentColor"
+                                                    strokeWidth="2"
+                                                    fill="none"
+                                                    strokeLinecap="round"
+                                                    strokeLinejoin="round"
+                                                >
+                                                    <path d="M5 12h14"></path>
+                                                    <path d="M12 5l7 7-7 7"></path>
+                                                </svg>
+                                            </a>
+                                        </Link>
+                                    </div>
+                                </div>
+                            </div>
+                        ))}
+                    </div>
+                </div>
+            </section>
+        </div>
+    );
+}
+
+const postQuery = groq`
+  *[_type == "post"]
+`;
+
+export async function getStaticProps() {
+    const post = await client.fetch(postQuery);
+    return {
+        props: {
+            data: { post },
+        },
+    };
+}
